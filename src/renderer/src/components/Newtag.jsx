72ece24a5e7c11f006e37134948252377ec234d1
Stanislas HOUETO <stanhoueto2002@gmail.com>
2024-05-19 14:54:13 +0100

    Fix: Select color in a list when creting tag

--- src/renderer/src/components/Newtag.jsx
+++ src/renderer/src/components/Newtag.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import "../assets/newtag.css";
 import { colorList } from "../utils/constants";
+import { FaCheck } from "react-icons/fa6";
 
 export function NewTag (props) {
   const [selectedColor, setSelectedColor] = useState(null);
-  let i = 0;
   return <div className="newtag">
-    <input className="tag-name" type="text" placeholder="Enter the tag name here" autoFocus={true}/>
+    <input className="tag-name" type="text" placeholder="Tag name" autoFocus={true}/>
     <div className="colorgrid">
       {
           colorList.map((element, index) => {
             return (
-              <div className="color" style={{ backgroundColor: element }}>
+              <div
+                onClick={() => setSelectedColor(element)}
+                className={"color" + (selectedColor==element ? " selected" : "")}
+                style={{ backgroundColor: element }}
+              >
+              {
+                selectedColor == element ?
+                  <FaCheck /> : null
+              }
               </div>
             )
           })
